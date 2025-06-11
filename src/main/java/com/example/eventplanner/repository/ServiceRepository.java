da42ff3cc31227053b94c1dd939e5dcd96cd9337
Roman <rart05@yahoo.com>
2025-06-11 13:20:14 +0200

    feat: added repositories for sevice and service-category

--- src/main/java/com/example/eventplanner/repository/ServiceRepository.java
+++ src/main/java/com/example/eventplanner/repository/ServiceRepository.java
@@ -1,10 +1,10 @@
 package com.example.eventplanner.repository;
 
-import com.example.eventplanner.model.ItemPhoto;
+import com.example.eventplanner.model.service.Service;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductPhotosRepository extends JpaRepository<ItemPhoto, Long> {
+public interface ServiceRepository extends JpaRepository<Service, Long> {
 
 }
