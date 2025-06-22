a8137bcc988ab4a44c73fa118ad075a8fefd42ea
Roman <rart05@yahoo.com>
2025-06-22 15:28:14 +0200

    refactor: controller methods signature

--- src/main/java/com/example/eventplanner/controller/FavoriteController.java
+++ src/main/java/com/example/eventplanner/controller/FavoriteController.java
@@ -5,14 +5,14 @@ import com.example.eventplanner.dto.eventDto.EventDto;
 import com.example.eventplanner.dto.product.ProductDTO;
 import com.example.eventplanner.dto.service.ProvidedServiceDTO;
 import com.example.eventplanner.service.FavoriteService;
+import com.example.eventplanner.service.JwtService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/favorites")
@@ -21,58 +21,68 @@ import java.util.List;
 public class FavoriteController {
 
     private final FavoriteService favoriteService;
+    private final JwtService jwtService;
 
     @PostMapping("/event/{eventId}")
-    public ResponseEntity<CommonMessageDTO> addFavoriteEvent(@PathVariable Long eventId, @RequestParam Long userId) {
-        CommonMessageDTO response = favoriteService.addFavoriteEvent(eventId, userId);
+    public ResponseEntity<CommonMessageDTO> addFavoriteEvent(@PathVariable Long eventId, HttpServletRequest request) {
+        String userEmail = jwtService.extractUserEmailFromAuthorizationRequest(request);
+        CommonMessageDTO response = favoriteService.addFavoriteEvent(eventId, userEmail);
         return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/event/{eventId}")
-    public ResponseEntity<CommonMessageDTO> removeFavoriteEvent(@PathVariable Long eventId, @RequestParam Long userId) {
-        CommonMessageDTO response = favoriteService.removeFavoriteEvent(eventId, userId);
+    public ResponseEntity<CommonMessageDTO> removeFavoriteEvent(@PathVariable Long eventId, HttpServletRequest request) {
+        String userEmail = jwtService.extractUserEmailFromAuthorizationRequest(request);
+        CommonMessageDTO response = favoriteService.removeFavoriteEvent(eventId, userEmail);
         return ResponseEntity.ok(response);
     }
 
     @PostMapping("/product/{productId}")
-    public ResponseEntity<CommonMessageDTO> addFavoriteProduct(@PathVariable Long productId, @RequestParam Long userId) {
-        CommonMessageDTO response = favoriteService.addFavoriteProduct(productId, userId);
+    public ResponseEntity<CommonMessageDTO> addFavoriteProduct(@PathVariable Long productId, HttpServletRequest request) {
+        String userEmail = jwtService.extractUserEmailFromAuthorizationRequest(request);
+        CommonMessageDTO response = favoriteService.addFavoriteProduct(productId, userEmail);
         return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/product/{productId}")
-    public ResponseEntity<CommonMessageDTO> removeFavoriteProduct(@PathVariable Long productId, @RequestParam Long userId) {
-        CommonMessageDTO response = favoriteService.removeFavoriteProduct(productId, userId);
+    public ResponseEntity<CommonMessageDTO> removeFavoriteProduct(@PathVariable Long productId, HttpServletRequest request) {
+        String userEmail = jwtService.extractUserEmailFromAuthorizationRequest(request);
+        CommonMessageDTO response = favoriteService.removeFavoriteProduct(productId, userEmail);
         return ResponseEntity.ok(response);
     }
 
     @PostMapping("/service/{serviceId}")
-    public ResponseEntity<CommonMessageDTO> addFavoriteService(@PathVariable Long serviceId, @RequestParam Long userId) {
-        CommonMessageDTO response = favoriteService.addFavoriteService(serviceId, userId);
+    public ResponseEntity<CommonMessageDTO> addFavoriteService(@PathVariable Long serviceId, HttpServletRequest request) {
+        String userEmail = jwtService.extractUserEmailFromAuthorizationRequest(request);
+        CommonMessageDTO response = favoriteService.addFavoriteService(serviceId, userEmail);
         return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/service/{serviceId}")
-    public ResponseEntity<CommonMessageDTO> removeFavoriteService(@PathVariable Long serviceId, @RequestParam Long userId) {
-        CommonMessageDTO response = favoriteService.removeFavoriteService(serviceId, userId);
+    public ResponseEntity<CommonMessageDTO> removeFavoriteService(@PathVariable Long serviceId, HttpServletRequest request) {
+        String userEmail = jwtService.extractUserEmailFromAuthorizationRequest(request);
+        CommonMessageDTO response = favoriteService.removeFavoriteService(serviceId, userEmail);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/event/my")
-    public ResponseEntity<Page<EventDto>> getMyFavoriteEvents(@RequestParam Long userId, Pageable pageable) {
-        Page<EventDto> favoriteEvents = favoriteService.getMyFavoriteEvents(userId);
+    public ResponseEntity<Page<EventDto>> getMyFavoriteEvents(HttpServletRequest request, Pageable pageable) {
+        String userEmail = jwtService.extractUserEmailFromAuthorizationRequest(request);
+        Page<EventDto> favoriteEvents = favoriteService.getFavoriteEvents(userEmail, pageable);
         return ResponseEntity.ok(favoriteEvents);
     }
 
     @GetMapping("/product/my")
-    public ResponseEntity<Page<ProductDTO>> getMyFavoriteProducts(@RequestParam Long userId, Pageable pageable) {
-        Page<ProductDTO> favoriteProducts = favoriteService.getMyFavoriteProducts(userId, pageable);
+    public ResponseEntity<Page<ProductDTO>> getMyFavoriteProducts(HttpServletRequest request, Pageable pageable) {
+        String userEmail = jwtService.extractUserEmailFromAuthorizationRequest(request);
+        Page<ProductDTO> favoriteProducts = favoriteService.getFavoriteProducts(userEmail, pageable);
         return ResponseEntity.ok(favoriteProducts);
     }
 
     @GetMapping("/service/my")
-    public ResponseEntity<Page<ProvidedServiceDTO>> getMyFavoriteServices(@RequestParam Long userId, Pageable pageable) {
-        Page<ProvidedServiceDTO> favoriteServices = favoriteService.getMyFavoriteServices(userId, pageable);
+    public ResponseEntity<Page<ProvidedServiceDTO>> getMyFavoriteServices(HttpServletRequest request, Pageable pageable) {
+        String userEmail = jwtService.extractUserEmailFromAuthorizationRequest(request);
+        Page<ProvidedServiceDTO> favoriteServices = favoriteService.getFavoriteServices(userEmail, pageable);
         return ResponseEntity.ok(favoriteServices);
     }
 
