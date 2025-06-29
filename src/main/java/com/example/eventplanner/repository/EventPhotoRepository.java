44b14dea5c8325dbf3ba1ee1b7fd5eafe63b13e9
Roman <rart05@yahoo.com>
2025-06-29 14:42:00 +0200

    feat: added new repository methods

--- src/main/java/com/example/eventplanner/repository/EventPhotoRepository.java
+++ src/main/java/com/example/eventplanner/repository/EventPhotoRepository.java
@@ -6,10 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface EventPhotoRepository extends JpaRepository<EventPhoto, Long> {
 
-    List<EventPhoto> findAllByEvent(Event event);
+    Optional<List<EventPhoto>> findAllByEvent(Event event);
 
 }
--- src/main/java/com/example/eventplanner/repository/EventRepository.java
+++ src/main/java/com/example/eventplanner/repository/EventRepository.java
@@ -11,7 +11,8 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
+
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long>, JpaSpecificationExecutor<Event> {
@@ -21,4 +22,6 @@ public interface EventRepository extends JpaRepository<Event, Long>, JpaSpecific
     Page<Event> findAllByOrderByLikesCountDesc(Pageable pageable);
 
     Page<Event> findAllByNameContainingIgnoreCaseOrDescriptionContainingIgnoreCaseAndPrivate(String name, String description, Pageable pageable, boolean isPrivate);
+
+    Page<Event> findAllByOrganizer(User user, Pageable pageable);
 }
