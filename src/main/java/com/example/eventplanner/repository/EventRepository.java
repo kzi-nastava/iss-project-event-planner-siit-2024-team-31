72a1921d2882747fc305995687f507f45d141024
Roman <rart05@yahoo.com>
2025-07-02 12:28:14 +0200

    feat: added Repositories for new Entities

--- src/main/java/com/example/eventplanner/repository/EventRepository.java
+++ src/main/java/com/example/eventplanner/repository/EventRepository.java
@@ -3,17 +3,16 @@ package com.example.eventplanner.repository;
 import com.example.eventplanner.model.Status;
 import com.example.eventplanner.model.event.Event;
 import com.example.eventplanner.model.user.User;
-import org.hibernate.annotations.processing.SQL;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.Instant;
 import java.util.List;
-import java.util.Optional;
 
 
 @Repository
@@ -27,7 +26,18 @@ public interface EventRepository extends JpaRepository<Event, Long>, JpaSpecific
 
     Page<Event> findAllByOrganizer(User user, Pageable pageable);
 
+    List<Event> findAllByOrganizer(User user);
+
     List<Event> findAllByInvites_UserAndStartTimeBetween(User user, Instant start, Instant end);
 
     List<Event> findAllByOrganizerAndStartTimeBetween(User organizer, Instant startOfMonth, Instant endOfMonth);
+
+    @Query("SELECT COUNT(e) FROM Event e WHERE e.status.name = :status")
+    Long countByStatusName(@Param("status") String status_name);
+
+    @Query("SELECT e FROM Event e LEFT JOIN FETCH e.registrations LEFT JOIN FETCH e.ratings")
+    List<Event> findAllWithAnalyticsData();
+
+    List<Event> findByOrganizerId(Long organizerId);
+
 }
--- src/main/java/com/example/eventplanner/repository/UserRepository.java
+++ src/main/java/com/example/eventplanner/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.example.eventplanner.repository;
 
 import com.example.eventplanner.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,4 +17,11 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     List<User> id(Long id);
+
+    @Query("SELECT DISTINCT u FROM User u " +
+            "JOIN EventRegistration er ON u.id = er.user.id " +
+            "JOIN EventAttendance ea ON u.id = ea.user.id " +
+            "WHERE er.event.id = :eventId AND ea.status.name = 'CLOSED'")
+    List<User> findAttendeesByEventId(@Param("eventId") Long eventId);
+
 }
